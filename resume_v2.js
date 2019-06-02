1f7b2e853912f64fdcdf440c5ae3ca31fd8ec34b
HanKyungSung <gksruddlakstp@gmail.com>
2019-06-02 03:57:22 -0500

    Version 2 80% done. All the content need to be replace.

--- resume_v2.js
+++ resume_v2.js
@@ -3,3 +3,15 @@ jQuery('.skillbar').each(function(){
     width:jQuery(this).attr('data-percent')
     },1500);
 });
+
+function topFunction()
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function moveToView(destination)
+{
+    var element = document.getElementById(destination);
+    element.scrollIntoView();
+}
